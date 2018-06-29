e0d5a516c87cd1079a18ee96ad9d3609306cf32d
sabinechang <sabinechang@liontech.com.tw>
2018-06-29 16:56:06 +0800

    Add header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <v-header></v-header>
+    <!-- <img src="./assets/logo.png">
+    <router-view/> -->
   </div>
 </template>
 
 <script>
+import header from './components/header.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "./src/sass/common.sass"
+#app
+  font-family: 'Muli', sans-serif
+  color: #50514f
+  ul
+    margin: 0
+    padding: 0
+    li
+      list-style: none
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <div class="header_inner">
+            <div class="logo">Logo</div>
+            <nav>
+                <ul>
+                    <li>About Me</li>
+                    <li>Webs</li>
+                    <li>Plugins</li>                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style lang="sass" scoped>
+    header
+        background-color: #f9f9f9
+        
+        .header_inner
+            max-width: 1200px
+            display: flex
+            justify-content: space-between
+            padding: 20px
+            margin: 0 auto
+            nav
+                color: #ff1654
+                font-size: 17px
+                ul
+                    display: flex
+                    li
+                        padding-left: 20px
+                        padding-right: 20px
+                        cursor: pointer
+                        &:hover
+                            text-decoration: underline
+</style>
+
