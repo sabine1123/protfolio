04de3e0e275338a5fd602d9676797013b7b6feee
sabinechang <sabinechang@liontech.com.tw>
2018-07-02 18:04:12 +0800

    Add bs3 & jq, and add callaps in plugin3

--- src/components/plugins.vue
+++ src/components/plugins.vue
@@ -155,19 +155,17 @@
             </div>
         </div>
         <div class="box">
-            <h1 class="title">3.
-                <a href="https://sabine1123.github.io/module-banner/">
-                    <span>Toggle banner</span>
-                </a>
-            </h1>
-            <div class="content">
+            <h2 class="title" data-toggle="collapse" href="#banner">
+                + Toggle banner
+            </h2>
+            <div class="content panel-collapse collapse" id="banner">
                 <div class="pic m-t-4">
                     <a href="https://sabine1123.github.io/module-banner/">
                         <img src="../assets/banner.png" alt="demo web">
                     </a>
                 </div>
                 <div class="text">
-                    <h2>Usage</h2>
+                    <h3>Usage</h3>
                     <!-- <div class="usage">
                         <div class="subtitle">
                             HTML
@@ -244,6 +242,22 @@
                 </div>
             </div>
         </div>
+        <!-- <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Collapsible Group Item #1
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+        </div> -->
     </div> <!-- wrap -->
 </template>
 
@@ -260,8 +274,10 @@
 .title
     margin-bottom: 0
     font-weight: 400
-    span
-        color: $mainR
+    color: $mainR
+    cursor: pointer
+    // span
+        
 .content
     // display: flex
 
