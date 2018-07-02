26da8aa1e205c8b9999017c074d84a667c3b3780
sabinechang <sabinechang@liontech.com.tw>
2018-07-02 15:09:22 +0800

    add plugin3

--- src/components/plugins.vue
+++ src/components/plugins.vue
@@ -9,8 +9,8 @@
             <div class="content">
                 <div class="pic">
                     <a href="https://sabine1123.github.io/module-calendar/preview.html">
-                        <img src="../assets/calendar.png" alt="demo web">
-                        <img src="../assets/calendar2.png" alt="demo web">
+                        <img class="c-pic" src="../assets/calendar.png" alt="demo web">
+                        <img class="c-pic" src="../assets/calendar2.png" alt="demo web">
                     </a>
                 </div>
                 <div class="text" id="calendar">
@@ -107,12 +107,13 @@
                 </a>
             </h1>
             <div class="content">
-                <div class="pic">
+                <div class="pic m-t-4">
                     <a href="https://sabine1123.github.io/module-flighttable/preview.html">
-                        <img src="../assets/frtable.png" alt="demo web">
+                        <img class="f-pic" src="../assets/frtable.png" alt="demo web">
+                        <img class="f-pic" src="../assets/frtable2.png" alt="demo web">
                     </a>
                 </div>
-                <div class="text" id="calendar">
+                <div class="text">
                     <h2>Usage</h2>
                     <table>
                         <thead>
@@ -153,15 +154,107 @@
                 </div>
             </div>
         </div>
+        <div class="box">
+            <h1 class="title">3.
+                <a href="https://sabine1123.github.io/module-banner/">
+                    <span>Toggle banner</span>
+                </a>
+            </h1>
+            <div class="content">
+                <div class="pic m-t-4">
+                    <a href="https://sabine1123.github.io/module-banner/">
+                        <img src="../assets/banner.png" alt="demo web">
+                    </a>
+                </div>
+                <div class="text">
+                    <h2>Usage</h2>
+                    <!-- <div class="usage">
+                        <div class="subtitle">
+                            HTML
+                        </div>
+                        <textarea>
+                            <div class="banner">
+                                <a class="wrap" href="#">
+                                    <img class="img" src="./imgs/1200x380.png" title="" alt="">
+                                </a>
+                            </div>
+                        </textarea>
+                        <div class="subtitle">
+                            Javascript
+                        </div>
+                        <textarea>
+                            $('.banner').banner({
+                                openAtStart: true,
+                                button: {
+                                    closeText: 'close', 
+                                    openText: 'open', 
+                                    class: 'btn' 
+                                },
+                                transition: true,
+                                autoToggle: true
+                            });
+                        </textarea>
+                    </div> -->
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Name</td>
+                                <td>Type</td>
+                                <td>Description</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>openAtStart</td>
+                                <td>boolean</td>
+                                <td>Setting the default display, opened or closed. Default: true.
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>autoToggle</td>
+                                <td>boolean | number</td>
+                                <td>Setting auto toggle. Default: true.</td>
+                            </tr>
+                            <tr>
+                                <td>button {closeText, openText, class}</td>
+                                <td>string</td>
+                                <td>Setting the button text and class.
+                                    <ul class="code">
+                                        <li>class: {</li>
+                                        <li>closed: 'closed'</li>
+                                        <li>closing: 'closing'</li>
+                                        <li>opened: 'opened', </li>
+                                        <li>opening: 'opening'</li>
+                                        <li>}</li>
+                                    </ul> 
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>transition</td>
+                                <td>boolean | number</td>
+                                <td>Is there the transition effect. Default: true.</td>                                
+                            </tr>
+                            <tr>
+                                <td>whenTransition</td>
+                                <td>function</td>
+                                <td>whenTransition: function()</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div> <!-- wrap -->
 </template>
 
 <style lang="sass" scoped>
 @import "./src/sass/common.sass"
+.box-wrap
+    // max-width: 80%
 .box 
     margin-top: 50px
     padding-bottom: 50px
-    background-color: #eee
+
 
 
 .title
@@ -170,24 +263,43 @@
     span
         color: $mainR
 .content
-    display: flex
+    // display: flex
 
+// .pic
+//     max-width: 35%
+//     margin-right: 100px
+//     margin-top: 30px
+//     img
+//         width: 100%
+//         margin-bottom: 20px
 .pic
-    max-width: 35%
-    margin-right: 100px
-    margin-top: 30px
-    img
-        width: 100%
-        margin-bottom: 20px
+    // max-width: 80%
+    // background: #fcc
+    a
+        display: flex
+        align-items: flex-start
+        img
+            max-width: 100%
+        .c-pic
+            width: 49%
+            &:first-child
+                margin-right: 2%
+        .f-pic
+            &:first-child
+                width: 60%                
+            &:last-child
+                width: 40%
+                margin-top: 5px
 
 .text
-    max-width: 65%
+    // max-width: 65%
     line-height: 1.5
     h2
         margin-bottom: 20px
     table
         border-collapse: collapse
-        max-width: 530px
+        // max-width: 530px
+        width: 100%
     thead
         font-weight: 600
         tr
@@ -204,10 +316,27 @@
             font-weight: 600
         
         .code
+            color: #999
+            font-style: italic
             li
                 padding-left: 15px
                 &:first-child, &:last-child
                     padding-left: 0
+    .subtitle
+        font-weight: 600
+        margin-bottom: 10px
+    textarea
+        width: 100%
+        min-height: 150px
+        appearance: none
+        border: none
+        outline: none
+        overflow: hidden
+        color: $mainG
+        line-height: 1.5
+        background-color: $secG
+        padding: 10px
+    
             
 
 
